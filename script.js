41ef2463158db3143ce25c0fe0f82c1beace42f4
Luan Honorio <110117312+luan-honorio@users.noreply.github.com>
2023-05-20 12:58:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let likes = 0;
-
-
 document.querySelector('.bt').addEventListener('click',()=>{
     
     document.querySelector('.btn').style.border = "solid 1px red";
-    document.querySelector('.btn').style.color = "red";
+    document.querySelector('.cor ').style.color = "red";
 
     likes++;
     document.querySelector("#like").innerHTML = likes;
 })
+
